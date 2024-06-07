ac8f7943d5a6ecc9662f94ea569ff5d61d5b1c74
atik.rony <atik.rony2013@gmail.com>
2024-06-08 03:02:42 +0600

    just started the weather app...will finish this within 4 to 5 days

---
+++ lib/view/screen_wether.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class WeatherScreen extends StatelessWidget {
+  const WeatherScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: Column(children: [
+              
+          ],),
+        ),
+      ),
+    );
+  }
+}
