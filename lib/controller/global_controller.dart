2db0e95fd2db78ea252db1aa61a5987c791c4a22
atik <atik.rony2013@gmail.com>
2024-06-11 00:13:27 +0600

    controller

---
+++ lib/controller/global_controller.dart
@@ -0,0 +1,7 @@
+import 'package:get/get.dart';
+
+class GlobalController extends GetxController {
+  final RxBool _isLoading = true.obs;
+  final RxDouble _lattitude = 0.0.obs;
+  final RxDouble _longitude = 0.0.obs;
+}
