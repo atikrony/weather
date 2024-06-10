bf9d1ee3b33f1fbef7d562a2c7a5892a6c1f6797
atik <atik.rony2013@gmail.com>
2024-06-11 00:19:01 +0600

    controller

--- lib/controller/global_controller.dart
+++ lib/controller/global_controller.dart
@@ -4,4 +4,18 @@ class GlobalController extends GetxController {
   final RxBool _isLoading = true.obs;
   final RxDouble _lattitude = 0.0.obs;
   final RxDouble _longitude = 0.0.obs;
+
+  RxBool checkLoading() => _isLoading;
+  RxDouble getLattitude() => _lattitude;
+  RxDouble getLongitude() => _longitude;
+
+  @override
+  void onInit() {
+    if (_isLoading.isTrue) {
+      getLocation();
+    }
+    super.onInit();
+  }
+
+  getLocation() async {}
 }
